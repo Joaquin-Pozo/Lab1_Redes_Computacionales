42a98ed92fbfda303fcf6f93dc7ba9cd6d50c71f
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-10-02 18:26:58 -0300

    Implementación de servidor TCP para el Centro de Control.

---
+++ centro_control.py
@@ -0,0 +1,29 @@
+# Servidor TCP - Centro de Control (Recibe reportes y los almacena en un historial)
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8001
+historial = []
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+    servidor.bind((HOST, PORT))
+    servidor.listen(1)
+    print("CONTROL: Esperando conexión de la Estación Espacial...")
+
+    conn, addr = servidor.accept()
+    with conn:
+        print(f'CONTROL: Conectado con {addr}')
+        while True:
+            data = conn.recv(1024).decode('utf-8')
+            if not data:
+                break
+            if data.startswith("REPORTE:"):
+                reporte = data.split("REPORT:")[1].strip()
+                historial.append(reporte)
+                conn.sendall("Report almacenado. Todo bajo control, Estación.\n".encode('utf-8'))
+            elif data.strip() == "CONSULTAR":
+                registros = "=== HISTORIAL DE COMUNICACIONES ===\n" + "\n".join(historial)
+                conn.sendall(registros.encode('utf-8'))
+            elif data.strip() == "MISION_COMPLETA":
+                conn.sendall("Comunicación finalizada. Buen trabajo, astrounautas.\n".encode('utf-8'))
+                break
