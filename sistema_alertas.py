5a8c484276d27994e04c46d6486ac82a06765381
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-10-02 18:33:18 -0300

    Implementación de servidor UDP para el Sistema de Alertas.

---
+++ sistema_alertas.py
@@ -0,0 +1,18 @@
+# Servidor UDP - Sistema de Alertas (Recibe alertas y responde inmediatamente sin almacenar información)
+import socket
+
+HOST = '127.0.0.1'
+PORT = 8002
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as servidor:
+    servidor.bind((HOST, PORT))
+    print("ALERTAS: Esperando alertas rápidas...")
+
+    while True:
+        data, addr = servidor.recvfrom(1024)
+        mensaje = data.decode('utf-8')
+        if mensaje == "base_segura":
+            print("Sistema: Modo emergencia desactivo. Mantente seguro allá arriba.")
+            break
+        respuesta = print(f'Alerta: CONFIRMADO: {mensaje}')
+        servidor.sendto(respuesta.encode('utf-8', addr))
